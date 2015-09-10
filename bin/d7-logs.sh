ad4eda4ae14e0d46fdaa4f0e3e8cd6f9e3f5b069
Reinier Vegter <hr.vegter@gmail.com>
2015-09-10 12:06:46 +0200

    Revert "Revert "d7-logs fix (num of lines), default.vcl fix""
    This reverts commit 1fcf68917049919d864907e399c84c59468bf289.

--- bin/d7-logs.sh
+++ bin/d7-logs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$1" != "" ]; then
+  tail=$1
+else
+  tail=100
+fi
+
 mydir=$(cd `dirname $(realpath "${BASH_SOURCE[0]}")` && pwd)
 name='d7'$(pwd | sed 's| |_|g' | sed 's|/|.|g')
-docker logs -f ${name}
+docker logs --tail=$tail -f ${name}
+
