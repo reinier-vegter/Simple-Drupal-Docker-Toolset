c44c7952fd63714bdf79914ffeddbfb5d5844073
Reinier Vegter <hr.vegter@gmail.com>
2016-01-29 16:37:52 +0100

    d7-drush fix

--- bin/d7-drush.sh
+++ bin/d7-drush.sh
@@ -5,4 +5,4 @@ mydir=$(cd `dirname "$script"` && pwd -P)
 . ${mydir}/common.sh
 
 cmd="cd /var/www; drush $@"
-docker exec -i -u www-data -t ${d7_container_name} bash -c "$cmd"
+docker exec -i -u $(whoami) ${d7_container_name} bash -c "$cmd"
